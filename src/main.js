4976691093147b9cf6cd76b2d1e8848d40555411
姜兴林 <jiangxinglin@limikeji.com>
2019-03-30 17:54:50 +0800

    添加babel、

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router'
 // axios封装引入
 import srv from '@/axios/api'
-
+import "babel-polyfill"
 Vue.config.productionTip = false
 Vue.prototype.srv = srv;
 
